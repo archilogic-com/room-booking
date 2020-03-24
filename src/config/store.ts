6e9cc3b7dc830768e247e220dacec36fab862702
Pablo Gancharov <pablogancharov@gmail.com>
2020-03-24 18:58:35 -0300

    updated store to not use devtools

--- src/config/store.ts
+++ src/config/store.ts
@@ -11,5 +11,6 @@ export const store = createStore(
     reducers,
     compose(
         applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+        // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        )
     )

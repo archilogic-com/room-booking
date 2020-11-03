f047919c4761acff7db38be07bfe787e45edab9a
davidhojman <43155033+davidhojman@users.noreply.github.com>
2020-11-03 10:43:07 -0300

    warning (#9)

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux'
-import {store} from 'config/store'
+import { store } from 'config/store'
 import * as serviceWorker from './serviceWorker';
 import axios from 'axios';
-axios.defaults.baseURL= process.env.REACT_APP_API_BASE_URL
+import Media from 'react-media';
+axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL
 axios.interceptors.request.use((config) => {
     config.params = config.params || {};
     config.params['token'] = process.env.REACT_APP_ARCHILOGIC_PUBLISHABLE_API_KEY;
-
     return config;
 }, (error) => {
     return Promise.reject(error);
@@ -18,9 +18,18 @@ axios.interceptors.request.use((config) => {
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <Media query="(max-width: 500px)" render={() =>
+            (
+                <div><img className='device-not-supported' src={require("./warning.png")} alt="" /></div>
+            )}
+        />
+        <Media query="(min-width: 501px)" render={() =>
+            (
+                <App />
+            )}
+        />
     </Provider>,
- document.getElementById('root'));
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
